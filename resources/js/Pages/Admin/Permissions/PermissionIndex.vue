c03b5e4cc9e510c57e62d83b0bf154e8210193c1
codewithtony <laraveller2021@gmail.com>
2023-03-02 14:21:14 +0100

    add table to index pages

--- resources/js/Pages/Admin/Permissions/PermissionIndex.vue
+++ resources/js/Pages/Admin/Permissions/PermissionIndex.vue
@@ -1,14 +1,45 @@
 <script setup>
 import AdminLayout from "@/Layouts/AdminLayout.vue";
 import { Head } from "@inertiajs/vue3";
+import Table from "@/Components/Table.vue";
+import TableRow from "@/Components/TableRow.vue";
+import TableHeaderCell from "@/Components/TableHeaderCell.vue";
+import TableDataCell from "@/Components/TableDataCell.vue";
+
+defineProps({
+  permissions: Object,
+});
 </script>
 
 <template>
   <Head title="Dashboard" />
 
   <AdminLayout>
-    <div class="py-4">
+    <div class="max-w-7xl mx-auto py-4">
       <h1>Permission Index Page</h1>
+      <div class="mt-6">
+        <Table>
+          <template #header>
+            <TableRow>
+              <TableHeaderCell>Id</TableHeaderCell>
+              <TableHeaderCell>Name</TableHeaderCell>
+              <TableHeaderCell>Email</TableHeaderCell>
+              <TableHeaderCell>Action</TableHeaderCell>
+            </TableRow>
+          </template>
+          <template #default>
+            <TableRow
+              v-for="permision in permisions"
+              :key="permision.id"
+              class="border-b border-slate-200"
+            >
+              <TableDataCell>{{ permision.id }}</TableDataCell>
+              <TableDataCell>{{ permision.name }}</TableDataCell>
+              <TableDataCell>Edit/Delete</TableDataCell>
+            </TableRow>
+          </template>
+        </Table>
+      </div>
     </div>
   </AdminLayout>
 </template>
